3c34b4d6ad795a6cc4dbdd1a5bc7a675d958e552
iskandarusmanov <usman29104@gmail.com>
2023-10-16 18:27:07 +0500

    done

---
+++ main.js
@@ -0,0 +1,30 @@
+// First task //
+
+// function firstTask(name, surname, age, birthDay, birthMonth ) {
+//     console.log("Argument 1:", name);
+//     console.log("Argument 2:", surname);
+//     console.log("Argument 3:", age);
+//     console.log("Argument 4:", birthDay);
+//     console.log("Argument 5:", birthMonth);
+//   }
+  
+//   firstTask("Iskandar","Usmonov","19","29","October");
+
+
+
+// Second task //
+
+// function secondTask() {
+//     const num1 = 10;
+//     const num2 = 20;
+//     const str1 = "Hello, ";
+//     const str2 = "world!";
+    
+//     return num1 + num2;     
+//     return num1 * num2;      
+//     return str1 + str2;         
+//     return num1 / num2;      
+//     return str2;               
+//   }
+  
+//   console.log(secondTask()); 
